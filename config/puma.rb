c25a6c1f8c0fd90da625d76e77fd535c3612f8c7
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-04-24 07:00:24 +0000

    edit pid path

--- config/puma.rb
+++ config/puma.rb
@@ -46,7 +46,7 @@ bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 rails_root = Dir.pwd
 
 if Rails.env.production?
-  pidfile File.join(rails_root, 'temp', 'pids', 'puma.pid')
+  pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
   state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
   stdout_redirect(
     File.join(rails_root, 'log', 'puma.log'),

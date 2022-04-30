21b20199f8ffff4f23dbea54b94101aada48fc76
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-04-30 07:47:09 +0000

    vendorのファイルを無視

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import '@fortawesome/fontawesome-free/js/all'
 import "chartkick/highcharts";
 import "jquery";
 import "bootstrap";
